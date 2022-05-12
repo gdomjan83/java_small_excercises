acaac7d8243035ad1671a1ce67bbe3a449d9fdd1
J_C <gomjan83@gmail.com>
2022-05-12 19:50:40 +0200

    Hello world test app

---
+++ spring-hello/src/main/java/webapp/ControllerConfig.java
@@ -0,0 +1,7 @@
+package webapp;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class ControllerConfig {
+}
---
+++ spring-hello/src/main/java/webapp/HelloController.java
@@ -0,0 +1,17 @@
+package webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello Gábor " + LocalDateTime.now();
+    }
+}
