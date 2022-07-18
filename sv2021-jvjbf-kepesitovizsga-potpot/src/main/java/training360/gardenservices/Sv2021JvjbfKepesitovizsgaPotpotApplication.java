b8e3a552c6356b32acfedb06f4059e9301b6924b
J_C <gomjan83@gmail.com>
2022-07-18 19:44:12 +0200

    Projekt init

---
+++ sv2021-jvjbf-kepesitovizsga-potpot/src/main/java/training360/gardenservices/Sv2021JvjbfKepesitovizsgaPotpotApplication.java
@@ -0,0 +1,13 @@
+package training360.gardenservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2021JvjbfKepesitovizsgaPotpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sv2021JvjbfKepesitovizsgaPotpotApplication.class, args);
+	}
+
+}
---
+++ sv2021-jvjbf-kepesitovizsga-potpot/src/test/java/training360/gardenservices/Sv2021JvjbfKepesitovizsgaPotpotApplicationTests.java
@@ -0,0 +1,13 @@
+package training360.gardenservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sv2021JvjbfKepesitovizsgaPotpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
