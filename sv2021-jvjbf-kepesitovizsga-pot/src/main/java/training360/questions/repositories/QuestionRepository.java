b016643510d1bca69795302b0cc91cb263fb6629
J_C <gomjan83@gmail.com>
2022-07-20 23:12:30 +0200

    Feladat kész

--- sv2021-jvjbf-kepesitovizsga-pot/src/main/java/training360/questions/repositories/QuestionRepository.java
+++ sv2021-jvjbf-kepesitovizsga-pot/src/main/java/training360/questions/repositories/QuestionRepository.java
@@ -1,10 +1,8 @@
 package training360.questions.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import training360.questions.dtos.NameQuestionPair;
 import training360.questions.model.Question;
-
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
